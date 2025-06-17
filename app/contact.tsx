b2bc3a67de8b61190a6e1dbc4b4cfd97ba4997fe
AidanGamble <gamble.aidan@gmail.com>
2025-06-17 13:40:30 -0700

    Added dummy pages for contact and FAQ page. Copied banner as a template for the MenuBar

---
+++ app/contact.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Banner from '../components/Banner';
+
+export default function ContactScreen() {
+
+  return (
+      <View style={styles.mainContainer}>
+        <Banner/>
+          <Text style={styles.welcomeText}>
+              this is the Contact screen
+          </Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+    },
+    welcomeText: {
+      color: "black", 
+      fontFamily: "Avenir Next",
+      fontWeight: "700", 
+      fontSize: 30, 
+      alignSelf: 'flex-start',
+      marginLeft: 30,
+    },
+    imageContainer: {
+      backgroundColor: '#D9E0F3',
+      width: "90%", 
+      height: "20%", 
+      borderRadius: 20,
+      overflow: 'hidden',
+      padding: 10,
+      marginBottom: 20,
+      alignItems: 'center', // Center horizontally
+      justifyContent: 'center'
+    },
+    image: {
+      width: "100%", // Make the image take full width
+      height: "100%", // Make the image take full height
+      resizeMode: "contain",
+    },
+    gridsContainer: {
+      backgroundColor: "White",
+      width: "70%", 
+      height: "12%", 
+      borderColor: "Black",
+      borderWidth: 1,
+      borderRadius: 20,
+      marginBottom: 20,
+      alignItems: 'center', // Center horizontally
+      justifyContent: 'center'
+    }
+  });
