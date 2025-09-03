5f4e1546722d52d5add35a9ffa5da27937131f32
AidanGamble <gamble.aidan@gmail.com>
2025-09-03 08:41:28 -0700

    Initial commit.

--- app/faq.tsx
+++ app/faq.tsx
@@ -1,21 +1,181 @@
-import MenuBar from '@/components/MenuBar';
 import * as React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, LayoutAnimation, Linking, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, UIManager, View } from 'react-native';
+import BackButton from '../components/BackButton';
 import Banner from '../components/Banner';
+import ContactUs from '../components/contactUs';
 
+// Enable animation on Android
+if (Platform.OS === 'android') {
+  UIManager.setLayoutAnimationEnabledExperimental &&
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
-export default function FAQScreen() {
+interface SelectedState {
+  eligibility: boolean;
+  appointments: boolean;
+  other: boolean;
+}
+
+interface ExpandedEligibilityState {
+  whoCanUse: boolean;
+  shortOnFood: boolean;
+  financialAid: boolean;
+  foodLimit: boolean;
+  itemLimit: boolean;
+  mCard: boolean;
+  volunteer: boolean;
+}
+
+interface ExpandedAppointmentState {
+  shop: boolean;
+  advanceAppointment: boolean;
+  scheduleAppointments: boolean;
+  appointmentSystem: boolean;
+  friend: boolean;
+  noLine: boolean;
+  howLong: boolean;
+  noAppointments: boolean;
+}
+
+type SectionKey = 'eligibility' | 'appointments' | 'other';
+type EligibilityKey = 'whoCanUse' | 'shortOnFood' | 'financialAid' | 
+  'foodLimit' | 'itemLimit' | 'mCard' | 'volunteer';
+type AppointmentsKey = 'shop' | 'advanceAppointment' | 'scheduleAppointments' 
+  | 'appointmentSystem' | 'friend' | 'noLine' | 'howLong' | 'noAppointments';
+type OtherKey = 'privacyPolicy';
+
+function FAQScreen() {
+
+  const [selected, setSelected] = React.useState<SelectedState>({
+      eligibility: true,
+      appointments: false,
+      other: false,
+    });
+
+  const [expanded, setExpandedEligibility] = React.useState<ExpandedState>({
+      eligibility: false,
+      appointments: false,
+      other: false,
+    });
+
+  const toggleSection = (section: SectionKey) => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setExpanded((prev) => ({ ...prev, [section]: !prev[section] }));
+  };
 
   return (
-      <View style={styles.mainContainer}>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          <Banner/>
-            <Text style={styles.welcomeText}>
-                this is the FAQ screen
+    <View style={styles.mainContainer}>
+      <ScrollView style={styles.mainContainer} >
+        <Banner/>
+        <BackButton/>
+        <View style={styles.content}>
+          <View style={{paddingHorizontal: 30}}>
+            <Text style={styles.heading}>
+              Frequently Asked Questions
             </Text>
-          </ScrollView>
-        <MenuBar/>
-      </View>
+          </View>
+          
+          <View style={styles.dropdownContainer}>
+            <View style={styles.dropdown}>
+              <TouchableOpacity onPress={() => toggleSection('personal')} style={styles.header}>
+                <Text style={styles.dropTitle}>Personal Care Items Donation List</Text>
+                <View style={styles.iconContainer}>
+                  <Image
+                    source={require("../assets/drive/downArrow.png")}
+                    style={[
+                      styles.icon,
+                      expanded.personal && { transform: [{ rotate: '180deg' }] }
+                    ]}
+                  />
+                </View>
+              </TouchableOpacity>
+              {expanded.personal && (
+                <View>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                </View>
+              )}
+            </View>
+            <View style={styles.dropdown}>
+              <TouchableOpacity onPress={() => toggleSection('spice')} style={styles.header}>
+                <Text style={styles.dropTitle}>Spice Donation List</Text>
+                <View style={styles.iconContainer}>
+                  <Image
+                    source={require("../assets/drive/downArrow.png")}
+                    style={[
+                      styles.icon,
+                      expanded.spice && { transform: [{ rotate: '180deg' }] }
+                    ]}
+                  />
+                </View>
+              </TouchableOpacity>
+              {expanded.spice && (
+                <View>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                </View>
+              )}
+            </View>
+            <View style={styles.dropdown}>
+              <TouchableOpacity onPress={() => toggleSection('cookware')} style={styles.header}>
+                <Text style={styles.dropTitle}>Cookware Donation List</Text>
+                <View style={styles.iconContainer}>
+                  <Image
+                    source={require("../assets/drive/downArrow.png")}
+                    style={[
+                      styles.icon,
+                      expanded.cookware && { transform: [{ rotate: '180deg' }] }
+                    ]}
+                  />
+                </View>
+              </TouchableOpacity>
+              {expanded.cookware && (
+                <View>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                  <Text>Lorem Ipsum</Text>
+                </View>
+              )}
+            </View>
+          </View>
+          <Text style={styles.bodyTextThird}>
+              {'\n'}
+              For access to our flyer materials, email
+              template, and drop off instructions, view
+              our information here:{' '}
+              <Text style={styles.hyperlink} onPress={() =>
+                  Linking.openURL('https://mbc.studentlife.umich.edu/how-to-help/hosting-a-donation-drive/')}>
+                  https://mbc.studentlife.umich.edu/how-to-help/hosting-a-donation-drive/
+              </Text>
+          </Text>
+          <Text style={styles.bottomParagraph}>
+            The Maize and Blue Cupboard utilizes volunteers
+            on a regular basis. If your group is interested in
+            volunteering at the Cupboard, please contact us
+            at{' '}
+              <Text style={styles.hyperlink} onPress={() => Linking.openURL('mailto:maize.blue.cupboard@umich.edu')}>
+                maize.blue.cupboard@umich.edu
+              </Text>
+          </Text>
+          <ContactUs/>
+          <Image
+            style={styles.mbcSignature}
+            source={require('../assets/mbc-signature-horizontal.png')}
+          />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
@@ -23,49 +183,125 @@ const styles = StyleSheet.create({
     mainContainer: {
       flex: 1,
       backgroundColor: '#fff',
+    },
+    content: {
       alignItems: 'center',
-      position: 'relative',
-      paddingBottom: 100,
-    },
-    welcomeText: {
-      color: "black", 
-      fontFamily: "Avenir Next",
-      fontWeight: "700", 
-      fontSize: 30, 
-      alignSelf: 'flex-start',
-      marginLeft: 30,
-    },
-    imageContainer: {
-      backgroundColor: '#D9E0F3',
-      width: "90%", 
-      height: "20%", 
-      borderRadius: 20,
-      overflow: 'hidden',
-      padding: 10,
-      marginBottom: 20,
-      alignItems: 'center', // Center horizontally
-      justifyContent: 'center'
+      paddingBottom: 105,
     },
     image: {
-      width: "100%", // Make the image take full width
-      height: "100%", // Make the image take full height
+      width: "100%",
+      height: "100%",
       resizeMode: "contain",
     },
-    gridsContainer: {
-      backgroundColor: "White",
-      width: "70%", 
-      height: "12%", 
-      borderColor: "Black",
-      borderWidth: 1,
-      borderRadius: 20,
-      marginBottom: 20,
-      alignItems: 'center', // Center horizontally
-      justifyContent: 'center'
+    heading: {
+      fontSize: 24,
+      fontFamily: "Montserrat_700Bold",
+      color: '#00274C',
+      textAlign: 'center',
     },
-    scrollContainer: {
-      flexGrow: 1,
-      backgroundColor: '#fff',
+    subHeading: {
+      fontSize: 16,
+      fontFamily: "Montserrat_700Bold",
+      color: '#00274C',
+      textAlign: 'center',
+      paddingTop: 35,
+    },
+    bodyTextFirst: {
+      fontSize: 14,
+      fontFamily: "Montserrat_400Regular",
+      color: '#000000',
+      textAlign: 'center',
+      paddingHorizontal: 57,
+      lineHeight: 19.6
+    },
+    bodyTextSecond: {
+      fontSize: 14,
+      fontFamily: "Montserrat_400Regular",
+      color: '#000000',
+      textAlign: 'center',
+      paddingHorizontal: 40,
+      paddingBottom: 32,
+      lineHeight: 19.6
+    },
+    bodyTextThird: {
+      fontSize: 14,
+      fontFamily: "Montserrat_400Regular",
+      color: '#000000',
+      textAlign: 'center',
+      paddingHorizontal: 55,
+      lineHeight: 19.6
+    },
+    bottomParagraph: {
+      textAlign: 'center',
+      fontSize: 12,
+      fontFamily: "Montserrat_500Medium",
+      paddingTop: 27,
+      paddingHorizontal: 55,
+      paddingBottom: 20,
+    },
+    hyperlink: {
+      textAlign: 'center',
+      fontSize: 13,
+      fontFamily: "Montserrat_500Medium",
+      paddingTop: 27,
+      paddingHorizontal: 40,
+      color: 'blue',
+      textDecorationLine: 'underline',
+    },
+    mbcSignature: {
+      width: '54.1%',
+      height: undefined,
+      aspectRatio: 211 / 28,
+      marginTop: 35,
+      resizeMode: "contain",
+    },
+    dropdownContainer: {
+      paddingTop: 19,
+      //Best practice is dead, I killed it
+      paddingBottom: -1,
+      paddingHorizontal: 16,
+      width: 346,
+      backgroundColor: '#002b5c',
+      flex: 1,
+      borderRadius: 10,
+    },
+    dropdown: {
+      backgroundColor: '#FFFFFF',
+      marginBottom: 20,
+      borderRadius: 6,
+      paddingHorizontal: 8,
+      paddingVertical: 13,
+      borderWidth: 2,
+      borderColor: '#FFCB05',
+    },
+    dropTitle: {
+      fontSize: 14,
+      fontFamily: "Montserrat_700Bold",
+      color: '#00274C',
+      textAlign: 'left',
+      flex: 1,
+    },
+    arrow: {
+      fontSize: 16,
+      color: '#002b5c',
+      marginLeft: 8,
+    },
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
       alignItems: 'center',
-      paddingBottom: 100,
+    },
+    iconContainer: {
+      width: 20,
+      height: 20,
+      paddingLeft: 5
+    },
+    icon: {
+      flex: 1,
+      width: null,
+      height: null,
+      resizeMode: 'contain'
     },
   });
+
+export default FAQScreen;
